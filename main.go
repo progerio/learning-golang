095eedf3bbdf63fc6d605c6d8c13e57e3e90e924
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2020-09-23 13:44:32 -0300

    Learning about types

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"org.progerio.com/learning/src/types"
+
 	"org.progerio.com/learning/src/structs"
 
 	"org.progerio.com/learning/src/functions"
@@ -23,12 +25,16 @@ func main() {
 	fmt.Println("..........Functions............")
 	fmt.Println("The sum from 10 + 20 = ", functions.Add(10, 20))
 	region, continent := functions.Location("LA")
-	fmt.Printf("Location : region = %s and continent = %s", region, continent)
+	fmt.Printf("Location : region = %s and continent = %s\n", region, continent)
 	fmt.Println("")
 
 	fmt.Println("..........Structs..............")
 	me := &structs.Artist{Name: "Paulo", Genre: "Software Enginer", Songs: 42}
 	fmt.Printf("%s realases their %dth song\n", me.Name, structs.NewRelease(me))
-	fmt.Printf("%s has a total of %d songs", me.Name, me.Songs)
+	fmt.Printf("%s has a total of %d songs\n", me.Name, me.Songs)
+	fmt.Println("")
+
+	fmt.Println("..........Types.................")
+	types.PrintTypes()
 	fmt.Println("")
 }
---
+++ src/types/types.go
@@ -0,0 +1,19 @@
+package types
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	goIsFun bool       = true
+	maxInt  uint64     = 1<<64 - 1
+	complex complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func PrintTypes() {
+	const f = "%T(%v)\n"
+	fmt.Printf(f, goIsFun, goIsFun)
+	fmt.Printf(f, maxInt, maxInt)
+	fmt.Printf(f, complex, complex)
+}
