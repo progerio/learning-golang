524f73f3db2a1c735280789f6435bee48eab300e
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2020-09-23 11:16:24 -0300

    Learning about Functions

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"org.progerio.com/learning/src/functions"
+
 	"org.progerio.com/learning/src/constants"
 	"org.progerio.com/learning/src/variables"
 )
@@ -14,6 +16,10 @@ func main() {
 
 	fmt.Println("..........Constants............")
 	constants.Constants()
+	fmt.Println("")
+
+	fmt.Println("..........Functions............")
+	fmt.Println("Soma de 10 + 20 = ", functions.Add(10, 20))
 	fmt.Println("")
 
 }
