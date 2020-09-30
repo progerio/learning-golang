2a72e5988dbacd9d5bf1e63c77d8bfcc259f56e1
Paulo Rogerio <paulo.rogeriobr@gmail.com>
2020-09-30 09:35:03 -0300

    Learning more about structs

--- main.go
+++ main.go
@@ -41,11 +41,18 @@ func main() {
 	x := new(structs.Bootcamp)
 	y := &structs.Bootcamp{}
 	fmt.Println(*x == *y)
-	user :=structs.User{ID: 42, Name: "Matt", Location: "LA"}
+	user := structs.User{ID: 42, Name: "Matt", Location: "LA"}
 	p := structs.Player{user, 90404}
 	fmt.Printf("Id: %d, Name: %s, Location: %s, Game id: %d\n", p.ID, p.Name, p.Location, p.Location)
 	p.ID = 11
 	fmt.Printf("%+v", p)
+
+	p2 := structs.Player{}
+	p2.ID = 42
+	p2.Name = "Matt"
+	p2.Location = "LA"
+	fmt.Println(p2.Greetings())
+
 	fmt.Println("")
 
 	fmt.Println("..........Types.................")
--- src/structs/user.go
+++ src/structs/user.go
@@ -1,7 +1,14 @@
 package structs
 
+import "fmt"
+
 // User struct
 type User struct {
 	ID             int
 	Name, Location string
+}
+
+// Greetings func
+func (u *User) Greetings() string {
+	return fmt.Sprintf("Hi %s from %s", u.Name, u.Location)
 }
